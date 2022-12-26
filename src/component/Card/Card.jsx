057f055ca4d993c7f9aa4801e9564979ac8c543e
Mohsin Muhammad Ali <mohsinali481997@gmail.com>
2022-12-26 13:01:44 +0500

    added v1

--- src/component/Card/Card.jsx
+++ src/component/Card/Card.jsx
@@ -7,9 +7,13 @@ import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import StarBorderOutlinedIcon from '@mui/icons-material/StarBorderOutlined';
+import alien from '../../assets/images/costume.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 export default function Cards() {
     return (
         <Card sx={{ maxWidth: 345, backgroundColor: 'black' }}>
+            {/* <CardContent sx={{}}> */}
             <CardMedia
                 sx={{ height: 140 }}
                 image={cap}
@@ -26,12 +30,33 @@ export default function Cards() {
                 <p>22D:22M</p>
             </Typography>
             <div className='Btns-Div'>
-                <Button variant='outlined' size="medium" className='detail-sell-btns'>DETAILS</Button>
-                <Button variant='outlined' size="medium" className='detail-sell-btns'>SELL</Button>
+                <Button variant='outlined' size="medium" className='detail-sell-btns detail-btn'>DETAILS</Button>
+                <Button variant='outlined' size="medium" className='detail-sell-btns sell-btn'>SELL</Button>
             </div>
-            <CardContent>
-                
-            </CardContent>
+            <div className='card-item-specification'>
+                <div>
+                    <p>Item</p>
+                    <p>#001</p>
+                </div>
+                <div>
+                    <p>Collection</p>
+                    <p>Allien Space Puffa
+                    </p>
+                    <FontAwesomeIcon icon={["fal", "coffee"]} />
+                </div>
+            </div>
+            <div className='card-item-specification'>
+                <div>
+                    <p>Launch Date</p>
+                    <p>11.11.22</p>
+                </div>
+                <div>
+                    <p>Total Minted</p>
+                    <p>100/1000</p>
+                </div>
+            </div>
+
+            {/* </CardContent> */}
             <CardActions>
             </CardActions>
         </Card>
